63feeca3acac2ae621a47a4335a8bf3b63705d17
KylePearce-SoftwareDeveloper <kylepearcesoftwaredeveloper@gmail.com>
2021-03-05 18:56:11 +0000

    #8 - Page navigation logic

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     path = path.substring(0, path.length - 5);
     var pathShort = path.substring(path.indexOf("/")+6);
 
-    if(pathShort == ""){
+    if(pathShort == "achmebroker"){
         console.log(pathShort);
         var anchor = document.getElementById(pathShort);
         var anchorParent = anchor.parentNode;
